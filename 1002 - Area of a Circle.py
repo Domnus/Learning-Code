bf4d842a35e8fb3837de493d071a0d90a10db563
Bento Carlos <52091442+Domnus@users.noreply.github.com>
2020-04-15 19:40:24 -0300

    Add files via upload

---
+++ 1002 - Area of a Circle.py
@@ -0,0 +1,5 @@
+R = float(input())
+
+A = (3.14159 * R**2)
+
+print('A={:.4f}'.format(A))
---
+++ 1009 - Salary with Bonus.py
@@ -0,0 +1,7 @@
+name = input()
+salary = float(input())
+sold = float(input())
+
+total = (sold * 0.15) + salary
+
+print('TOTAL = %.2f' %total)
