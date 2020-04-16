f777a2da7647f179d20f97ebee759799d9911272
Bento Carlos <bcsilva49@gmail.com>
2020-04-16 19:18:43 -0300

    masater

--- College.py/Exercises/Introdução à Programação de Computadores/teste.py
+++ College.py/Exercises/Introdução à Programação de Computadores/teste.py
@@ -1,46 +1,12 @@
-import random
-cont = 0
-cond = True
-cond2 = True
-while cond:
-    x = random.randint(100, 1000)
-    c = x // 100
-    d = (x % 100) // 10
-    u = x % 10
-    if c != d and c!= u and d != u:
-        cond = False
+par = 0
+impar = 0
+for i in range(0,10):
+    num = int(input('Digite um numero'))
+    if num%2==0:
+        par+=num
     else:
-        cond = True
-while cond2:
-    c = x // 100
-    d = (x % 100) // 10
-    u = x % 10
-    if c > d and c > u:
-      if d > u:
-            menor = (u*100) + (d * 10) + c
-            maior = (c * 100) + (d * 10) + u
-      else:
-            menor = (d * 100) + (u * 10) + c
-            maior = (c * 100) + (u * 10) + d
-    elif d > c and d > u:
-      if c > u:
-            menor = (u*100) + (c * 10) + d
-            maior = (d * 100) + (c * 10) + u
-      else:
-            menor = (u * 100) + (c * 10) + d
-            maior = (d * 100) + (u * 10) + c
-    elif u > c and u > d:
-      if c > d:
-            menor = (d*100) + (c * 10) + u
-            maior = (u * 100) + (c * 10) + d
-      else:
-            menor = (c * 100) + (d * 10) + u
-            maior = (u * 100) + (d * 10) + c
-    x = maior - menor
-    cont += 1
-    cond2 = x != 495
-
-print("Total de iterações : {}".format(cont))
+        impar+=num
+print('A soma dos pares é {} a soma dos impares é{}'.format(par, impar))
 
         
 
