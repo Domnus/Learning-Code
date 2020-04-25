5f26fc5f87dbc9155ea12c4f28ddcf53417f2f16
Bento Carlos <bcsilva49@gmail.com>
2020-04-25 20:53:20 -0300

    master

--- College.py/Exercises/Matemática Computacional/Aula09_Problema2.py
+++ College.py/Exercises/Matemática Computacional/Aula09_Problema2.py
@@ -1,9 +1,14 @@
-k = int(input('Função Resto -> '))
+k = int(input('Função Resto \n-> '))
 m = int(input('-> '))
 
-if k > 0:
-    result = k % m
-if k < 0:
-    result2 = (k * -1) % m
-    result = m - result2
-print(result)
+if m > 0:
+    if k > 0:
+        result = k % m
+    elif k < 0:
+        result2 = (k * -1) % m
+        result = m - result2
+    print(result)
+else:
+    print('Número inválido!')
+
+
