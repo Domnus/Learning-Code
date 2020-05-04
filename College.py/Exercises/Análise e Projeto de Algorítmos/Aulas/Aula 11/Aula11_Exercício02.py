4b180d3ad8164b94cbfaaf0d86551d3309a72617
Bento Carlos <bcsilva49@gmail.com>
2020-05-04 20:24:32 -0300

    master

--- College.py/Exercises/Análise e Projeto de Algorítmos/Aulas/Aula 11/Aula11_Exercício02.py
+++ College.py/Exercises/Análise e Projeto de Algorítmos/Aulas/Aula 11/Aula11_Exercício02.py
@@ -1,12 +1,22 @@
+print('-----20 primeiros números da série de Fibonacci-----')
+
+'''
 a = 0
 b = 1
 c = 1
 A= []
-print('-----20 primeiros números da série de Fibonacci-----')
+
 for i in range(20):
     A.append(c)
     c = a + b
     a = b
     b = c
-   
+'''
+A = [0]*20
+A[0] = 1
+A[1] = 1
+
+for i in range(2, 20):
+    A[i] = A[i - 1] + A[i - 2]
+
 print(A)
