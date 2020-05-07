808f720b5dcd1a1fbd9b2b699403b3049e7ed30d
Bento Carlos <bcsilva49@gmail.com>
2020-05-06 21:44:49 -0300

    master

--- College.py/Exercises/Introdução à Programação de Computadores/6ª Lista de Exercícios/Lista6_Exercício09.py
+++ College.py/Exercises/Introdução à Programação de Computadores/6ª Lista de Exercícios/Lista6_Exercício09.py
@@ -1,10 +1,11 @@
 from random import randint
 
 L = []
+i = 0
 
-for i in range(10):    
+while i < (10):    
     x = randint(1, 50)
     if x not in L:
         L.append(x)
-    
+        i += 1    
 print(L)
---
+++ College.py/Exercises/Introdução à Programação de Computadores/Trabalhos/av10.py
@@ -0,0 +1,44 @@
+from random import randint
+
+A = []
+B = []
+C = []
+i = 0
+y = True
+
+print('Digite os números desejados:')
+
+for j in range(6):
+    x = randint(1, 60)
+    if x not in B:
+        A.append(x)
+
+while i < 6:
+    x = int(input('-> '))
+    if x < 1 or x > 60:
+        print('Número inválido!')
+        continue
+    elif x in B:
+        print('Número já digitado!')
+    elif x not in B:
+        B.append(x)
+        i += 1
+
+A.sort()
+B.sort()
+
+for i in A:
+    if i in B:
+        C.append(i)
+
+print(f'Números sorteados: \n{A}')
+if len(C) == 4:
+    print(f'Parabéns! Você acertou a quadra! Esses foram os números que você acertou: \n{C}')
+elif len(C) == 5:
+    print(f'Parabéns! Você acertou a quina! Esses foram os números que você acertou: \n{C}')
+elif len(C) == 6:
+    print(f'Parabéns! Você acertou a sena! Esses foram os números que você acertou: \n{C}')
+elif len(C) == 0:
+    print('Infelimente você não acertou nenhum número! Mais sorte na próxima vez!')
+else:
+    print(f'Você acertou {len(C)} número(s)! Aqui estão eles: \n{C}')
