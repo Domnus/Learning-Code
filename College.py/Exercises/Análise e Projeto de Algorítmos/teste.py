29b40dff684b437fea9ed784707044b69ecb67aa
Bento Carlos <bcsilva49@gmail.com>
2020-05-11 20:10:24 -0300

    master

--- College.py/Exercises/Análise e Projeto de Algorítmos/teste.py
+++ College.py/Exercises/Análise e Projeto de Algorítmos/teste.py
@@ -1,24 +1,10 @@
-from random import randint
-a = 'sim'
-L = []
-while a == 'sim':
-    for i in range(10):
-        L.append(randint(1,50))
+matriz = [0] * 5
 
-    print('Digite 1 para mostrar em ordem normal \nDigite 2 para mostrar em ordem inversa')
+print('Digite os elementos da matriz: ')
 
-    while True:
-        x = int(input('Digite o numero: '))
-        if x == 1:
-            print(L)
-            break
-        elif x == 2:
-            L.reverse()
-            print(L)
-            break
-        else:
-            print('Numero invalido')
-            continue
+for i in range(5):
+    matriz[i] = [0] * 5
+    for j in range(5):
+        matriz[i][j] = int(input(''))
 
-    a = input('Deseja repetir? [Sim/Nao] ')
-    a = a.lower()
+print(matriz)
