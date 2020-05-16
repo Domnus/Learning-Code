34e2b173e9af029e1179baf9c28238f656f1f93d
Bento Carlos <bcsilva49@gmail.com>
2020-05-16 15:07:56 -0300

    master

--- College.py/Exercises/Introdução à Programação de Computadores/teste.py
+++ College.py/Exercises/Introdução à Programação de Computadores/teste.py
@@ -1,28 +1,4 @@
-from random import randint
-
-Lista = []
-l = []
-for i in range(50):
-    Lista.append(randint(1, 101))
-print('A lsita é',Lista)
-cond = True
-while cond:
-        x = int(input('Digite o valor de X:'))
-        if x > 49 or x < 0:
-            print('Digite um valor entre 0 e 49!')
-            continue 
-        else:
-            y = int(input('Digite o valor de Y:'))
-            if y > 49 or y < 0:
-                print('Digite um valor entre 0 e 49"!')  
-                continue
-            else:
-                if y < x:
-                    print('O valor de X deve ser menor que o Y!')
-                else:
-                    for i in range(x, y):
-                        posiçao = Lista[i]
-                        l.append(posiçao)
-                    cond = False
-    
-print(f'A lista nova é {l}')
+for i in range(5):
+    for j in range(1, 6):
+        print(j + i, end='')
+    print()
