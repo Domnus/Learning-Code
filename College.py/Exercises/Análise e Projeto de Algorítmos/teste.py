8c4b8d009fbc4ce146dbd10c077c9d3b8e3edd3e
Bento Carlos <bcsilva49@gmail.com>
2020-05-20 22:12:48 -0300

    master

--- College.py/Exercises/Análise e Projeto de Algorítmos/teste.py
+++ College.py/Exercises/Análise e Projeto de Algorítmos/teste.py
@@ -1,21 +1,12 @@
-from random import randint
-
-arr = [0] * 8
 v = 0
-
-for i in range(8):
-    arr[i] = [0] * 8
-    for j in range(8):
-        arr[i][j] = 0
-
-for i in range(8):
-    for j in range(8):
-        arr[i][j] = (i+j) % 2
-        
-print('Matriz:')
-print('=' * 24)
-for i in range(8):
-    for j in range(8):
-        print(f'[{arr[i][j]}]', end='')
+a = [0]*8
+for i in range (8):
+    a[i] = [0]*8
+    for j in range (8):
+        a[i][j] = v
+        v = 1-v
+    v = 1-v
+for i in range (8):
+    for j in range (8):
+        print(a[i][j], end = "  ")
     print()
-print('=' * 24)  
