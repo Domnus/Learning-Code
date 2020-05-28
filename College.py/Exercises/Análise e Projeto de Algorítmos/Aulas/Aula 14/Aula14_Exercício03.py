5a4466c79366d93f6bc2b16455c6176cd2aaedea
Bento Carlos <bcsilva49@gmail.com>
2020-05-28 20:32:10 -0300

    master

--- College.py/Exercises/Análise e Projeto de Algorítmos/Aulas/Aula 14/Aula14_Exercício03.py
+++ College.py/Exercises/Análise e Projeto de Algorítmos/Aulas/Aula 14/Aula14_Exercício03.py
@@ -1,15 +1,30 @@
-arr = [0] * 8
-board = {'A':0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H':7}
-
-for i in range(8):
-    arr[i] = [0] * 8
-    
+tab = [] *  8
+cont = 1
+lista = ['a','b','c','d','e','f','g','h']
+for i in range(0,8):
+    tab.append([])
+    print(' {}'.format(i),end=' ')
+print()
+for i in range(0,8):
+    print('{}'.format(i+1),end='')
+    for j in range(0,8):
+        print('[ ]',end='')
+    print()
 while True:
-    y, letra = input('Informe as coordenadas da Torre [1 a 8],[A a H] -> ').split(',')
-    letra = letra.upper()
-    x = board[letra]
+    x,y = input('Insira a posição que o cavalo sera inserido(letra,numero) - > ').split(',')
     y = int(y) - 1
-    if x not in range(8) or y not in range(8):
-        print('Coordenadas erradas! Digite um valor entre 0 e 7!')
-    else:
+    if x in lista and 0 <=  y <= 7:
         break
+for i in lista:
+    if i == x:
+        x = lista.index(i)
+for i in range(0,8):
+    for j in range(0,8):
+        if j == x and i == y:
+            print('[{:3}]'.format(' C'),end='')
+        elif (j == x+2 and i == y+1) or (j == x+1 and i == y+2) or (j == x-2 and i == y-1) or (j == x-1 and i == y-2) or (j == x+1 and i == y-2) or (j == x-2 and i == y+1) or (j == x-1 and i == y+2) or (j == x+2 and i == y-1):
+            print('[{:3}]'.format(' X'), end='')
+
+        else:
+            print('[{:3}]'.format(''),end='')
+    print()
