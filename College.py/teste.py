c4d258d4b7574222bb2cbad34145bb9de0d4c1b8
Bento Carlos <bcsilva49@gmail.com>
2020-06-02 16:32:50 -0300

    master

--- College.py/teste.py
+++ College.py/teste.py
@@ -1,29 +1,5 @@
-arr = [0] * 8
-board = {'A':0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H':7}
+l = []
 
 for i in range(8):
-    arr[i] = [0] * 8
-    
-while True:
-    letra, y = input('Informe as coordenadas da Torre [0 a 7],[A a H] -> ').split(',')
-    letra = letra.upper()
-    x = board[letra]
-    y = int(y)
-    if x not in range(8) or y not in range(8):
-        print('Coordenadas erradas! Digite um valor entre 0 e 7!')
-    else:
-        break
-
-for i in range(8):
-    for j in range(8):
-        if arr[i][j] == arr[x][y]:
-            arr[x][y] = 'T'
-        elif i == x:
-            arr[i][j] = 1
-        elif j == y:
-            arr[i][j] = 1
-
-for i in range(8):
-    for j in range(8):
-        print(f'[{arr[i][j]}]', end= '')
-    print()
+    l.append(input('-> '))
+print(l)
