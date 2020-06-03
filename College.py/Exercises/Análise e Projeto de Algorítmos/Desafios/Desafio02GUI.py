ca09686456f8e50c8b7b8ef45dc1bf47bcad2e1c
Bento Carlos <bcsilva49@gmail.com>
2020-06-03 13:35:49 -0300

    master

--- College.py/Exercises/Análise e Projeto de Algorítmos/Desafios/Desafio02GUI.py
+++ College.py/Exercises/Análise e Projeto de Algorítmos/Desafios/Desafio02GUI.py
@@ -1,38 +1,90 @@
 import tkinter as tk
+from random import randint as rd
 
 HEIGHT = 640
 WIDTH = 480
 
 root = tk.Tk()
 
+global imagem
+global h
+global w
+
 root.title('Desafio 2')
 
-def criar_matriz(entry):
-    imagem = []
-    h, w = input('Digite o tamanho da imagem [HxW] -> ').split('x')
+def inserir(h, w):
     h = int(h)
     w = int(w)
-    if h > 20 or w > 20:
-        print('A matriz não deve ultrapassar 20x20')
-    else:
-        print('Digite os valores da matriz:')
+    if h < 20 or w < 20:
         imagem = [0] * h
         for i in range(h):
             imagem[i] = [0] * w
             cond = 0
             while cond < w:
-                x = randint(0, 1)
+                x = rd(0, 1)
                 if x != 0 and x != 1:
                     print('Digite apenas 1 ou 0!')
                 else:
                     imagem[i][cond] = x
                     cond += 1
-    print('=' * len(imagem) * 2)        
     for i in range(h):
+        elem = tk.Label(upper_frame, textvariable=matriz, fg='red')
+        elem.pack()
+        
+
+def compactar():
+    compactacao = []
+    for i in range(h):
+        p, b = 0, 0
         for j in range(w):
-            print(f'{imagem[i][j]}', end=' ')
-            print()
-    print('=' * len(imagem) * 2)
+            if(imagem[i][j] == 1):
+                p += 1
+            if(imagem[i][j] == 0):
+                b += 1
+            if j == w-1 or imagem[i][j] != imagem[i][j+1]:
+                if(p>0):
+                    compactacao.append(f'{p}P')
+            elif(b>0):
+                compactacao.append(f'{b}B')    
+            p, b = 0, 0
+        compactacao.append('0')
+    compactacao.append('0')
+    compactado = tk.Label(upper_frame, text=compactacao, bg='blue', fg='white')
+    compactado.place(rely=0.8, relheight=0.1, relwidth=1)
+
+
+def descompactar(entry):
+    global arr
+    arr = [[]]
+    cont = 0
+    linhas = 1
+    linha_w = 0
+    for character in entry:
+        if character.isdecimal():
+            numero = int(character)
+        else:
+            letra = character
+        if numero == 0:
+            arr.append([])
+            cont += 1
+            linhas += 1
+            continue
+        try:
+            if letra == 'B':
+                for i in range(numero):
+                    arr[cont].append(0)
+            elif letra == 'P':
+                for i in range(numero):
+                    arr[cont].append(1)
+            letra = ''
+        except Exception:
+            pass
+        
+    for i in range(1):
+        arr.pop()
+
+    
+
 
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
@@ -40,20 +92,26 @@ canvas.pack()
 frame = tk.Frame(root, bg='#baad65')
 frame.place(relx=0, rely=0, relwidth=1, relheight=1)
 
-inserir = tk.Button(frame, text="1 – Inserir a imagem (criar a matriz (max. 20x20)", bg='black', fg='white')
-inserir.pack()
+inserir_1 = tk.Button(frame, text="1 – Inserir a imagem (criar a matriz (max. 20x20)", bg='black', fg='white', padx=30, command=lambda: inserir(height,width.get()))
+inserir_1.pack()
+
+height = tk.Entry(frame)
+height.pack()
+
+width = tk.Entry(frame)
+width.pack()
+
+compactar_2 = tk.Button(frame, text='2 – Compactar a imagem', bg='black', fg='white', padx=40.5, command=compactar)
+compactar_2.pack()
+
+descompactar_3 = tk.Button(frame, text='3 – Descompactar a imagem', bg='black', fg='white', padx=30, command=lambda: descompactar(entry.get()))
+descompactar_3.pack()
 
 entry = tk.Entry(frame)
 entry.pack()
 
-compactar = tk.Button(frame, text='2 – Compactar a imagem', bg='black', fg='white')
-compactar.pack()
-
-descompactar = tk.Button(frame, text='3 – Descompactar a imagem', bg='black', fg='white')
-descompactar.pack()
-
-sair = tk.Button(frame, text='4 – Sair do programa', bg='black', fg='white', command=quit)
-sair.pack()
+sair_4 = tk.Button(frame, text='4 – Sair do programa', bg='black', fg='white', command=quit, padx=53)
+sair_4.pack()
 
 upper_frame = tk.Frame(root, bg='#0bd46f', bd=10)
 upper_frame.place(relx=0.5, rely=0.3, relheight=0.55, relwidth=0.75, anchor='n')
@@ -61,5 +119,6 @@ upper_frame.place(relx=0.5, rely=0.3, relheight=0.55, relwidth=0.75, anchor='n')
 label = tk.Label(upper_frame, text='Matriz')
 label.pack()
 
+matriz = tk.StringVar()
 
 root.mainloop()
--- College.py/teste.py
+++ College.py/teste.py
@@ -1,5 +1,99 @@
-l = []
+from random import randint
 
-for i in range(8):
-    l.append(input('-> '))
-print(l)
+print("","=-" * 20, "\n           * IMAGEM BITMAP *\n", "=-" * 20, "\nOpções: 1-Criar Matriz; 2-Compactar Imagem; 3-Descompactar Imagem; 4-Sair do Programa")
+op = int(input("Selecione uma opção: "))
+while (op != 1 and op != 2 and op != 3 and op != 4):
+    op = int(input("NÚMERO INVÁLIDO! Digite um número entre 1 e 4: "))
+
+while op != 4:
+
+    if(op == 1):
+        resp = input("Criar a matriz manualmente (M) ou randomicamente (R): ").upper()[0]
+        while (resp != "M" and resp != "R"):
+            resp = int("Resposta Inválida! Digite M ou R: ")
+        l = int(input("Digite o número de linhas da matriz: "))
+        c = int(input("Digite o número de colunas da matriz: "))
+        matriz = [0] * l
+        VetCompact = []
+        cont = -1
+        if (resp == "M"):
+            print("\nCrie a Imagem:")
+            for i in range(l):
+                matriz[i] = [0] * c
+                for j in range(c):
+                    matriz[i][j] = int(input(f"Digite o valor (0 ou 1) da posição {i + 1}x{j + 1}: "))
+                    while (matriz[i][j] != 0 and matriz[i][j] != 1):
+                        matriz[i][j] = int(input(f"VALOR INVÁLIDO! Digite 0 ou 1 para a posição {i + 1}x{j + 1}: "))
+            print("\n\nAqui está a imagem que você criou:")
+        else:
+            for i in range (l):
+                matriz[i] = [0]*c
+                for j in range (c):
+                    matriz[i][j] = randint(0,1)
+            print("\nAqui está a imagem criada randomicamente:")
+        for i in range(l):
+            for j in range(c):
+                if matriz[i][j] == 1:
+                    print("\033[0:33:47m   \033[m", end="")  # 'pixel' CINZA
+                else:
+                    print("\033[0:33:40m   \033[m", end="")  # 'pixel' BRANCO
+            print()
+
+    if (op == 2):                                                                                                           # 2 = COMPACTAR IMAGEM
+        vetCompact = []
+        cont = -1
+        resp = input("\nUtilizar uma imagem pronta (P) ou Criar imagem manualmente (M): ").upper()[0]
+        if (resp == "M"):
+            l = int(input("Digite o número de linhas da matriz: "))
+            c = int(input("Digite o número de colunas da matriz: "))
+            matriz = [0]*l
+            for i in range(l):
+                matriz[i] = [0] * c
+                for j in range(c):
+                    matriz[i][j] = int(input(f"Digite o valor (0 ou 1) da posição {i + 1}x{j + 1}: "))
+                    while (matriz[i][j] != 0 and matriz[i][j] != 1):
+                        matriz[i][j] = int(input(f"VALOR INVÁLIDO! Digite 0 ou 1 para a posição {i + 1}x{j + 1}: "))
+
+        print(f"\nImagem Compactada: ", end="")
+        for i in range(l):
+            p, b = 0, 0
+            for j in range(c):
+                if (matriz[i][j] == 1):
+                    p += 1
+                if (matriz[i][j] == 0):
+                    b += 1
+                if j == c - 1 or matriz[i][j] != matriz[i][j + 1]:
+                    if (p > 0):
+                        vetCompact.append(f"{p}P")
+                        print(f"{p}P", end="")
+                        cont += 1
+                    elif (b > 0):
+                        vetCompact.append(f"{b}B")
+                        print(f"{b}B", end="")
+                        cont += 1
+                    p, b = 0, 0
+            vetCompact.append(0)
+            print("0", end="")
+            cont += 1
+        vetCompact.append(0)
+        print("0")
+        cont += 1
+
+
+    if (op == 3):
+        compac = input("Digite (ou cole) aqui a imagem compactada (apenas preto e branco): ")
+        print("\nImagem descompactada: ")
+
+        for i in range (len(compac)):
+            if (compac[i] == '0'):
+                print()
+            elif (compac[i].isnumeric()):
+                n = int(compac[i])
+            elif (compac[i] == "P"):
+                print("1  "*n, end = "")            # Obs: print("\033[0:33:47m \033[m" * n, end="") PARA BIT CINZA
+            elif (compac[i] == "B"):
+                print("0  "*n, end = "")            # Obs: print("\033[0:33:40m \033[m" * n, end="") PARA BIT BRANCO
+
+    op = int(input("\nSelecione uma das opções: 1-Criar Matriz; 2-Compactar Imagem; 3-Descompactar Imagem; 4-Sair do Programa: "))
+
+x = input("\nPrograma Encerrado\nPressione qualquer tecla para fechar a janela.")
