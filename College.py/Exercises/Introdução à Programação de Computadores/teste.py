5a2cc31e586641d54b0efc054bf92eed17ac0d90
Bento Carlos <bcsilva49@gmail.com>
2020-06-05 09:48:10 -0300

    master

--- College.py/Exercises/Introdução à Programação de Computadores/teste.py
+++ College.py/Exercises/Introdução à Programação de Computadores/teste.py
@@ -1,6 +1,19 @@
-from random import randint as rd
-
-num = rd(0, 6)
-cor = rd(0, 1)
-
+p = 0
+k = 0
+a = int(input(f'Digite a idade do primeiro homem = '))
+b = int(input(f'Digite a idade do segundo homem = '))
+c = int(input(f'Digite a idade da primeira mulher = '))
+d = int(input(f'Digite a idade da segunda mulher = '))
+if b > a:
+    p = a
+    a = b
+    b = p
+if d > c:
+    k = c
+    c = d
+    d = k
+soma1 = a + d
+soma2 = b + c
+print(f'Homem mais velho + mulher mais nova = {soma1}')
+print(f'Mulher mais velha + homem mais novo = {soma2}')
 
--- College.py/teste.py
+++ College.py/teste.py
@@ -1,11 +1,4 @@
-def myfunc(*args):
-    even = []
-    mylist = list(args)
-    for number in mylist:
-        if number % 2 == 0:
-            even.append(number)
-    print(even)
-    
-myfunc(5,6,7,8)
-    
+n = 12663989 / 983
+
+print(n)
 
