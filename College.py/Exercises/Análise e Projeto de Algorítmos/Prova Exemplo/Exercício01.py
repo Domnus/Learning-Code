ba25aeb1744a4ef48fa336c99d29b8280a2651ea
Bento Carlos <bcsilva49@gmail.com>
2020-06-08 20:44:22 -0300

    master

--- College.py/Exercises/Análise e Projeto de Algorítmos/Prova Exemplo/Exercício01.py
+++ College.py/Exercises/Análise e Projeto de Algorítmos/Prova Exemplo/Exercício01.py
@@ -11,7 +11,10 @@ print('=' * 80)
 print(arr)
 print('=' * 80)
 
-arr.sort()
+for i in range(20):
+    for j in range(i+1, 20):
+        if arr[j] < arr[i]:
+            arr[j], arr[i] = arr[i], arr[j]
 
 print(f'As duas menores notas são: {arr[0]} e {arr[1]}')
 
