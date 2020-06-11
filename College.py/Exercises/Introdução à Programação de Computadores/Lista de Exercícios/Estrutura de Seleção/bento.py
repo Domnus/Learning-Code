0637cd6448fc8115fc708f3ebe753a65340d25bf
Bento Carlos <bcsilva49@gmail.com>
2020-06-10 21:27:03 -0300

    master

--- College.py/Exercises/Introdução à Programação de Computadores/Lista de Exercícios/Estrutura de Seleção/bento.py
+++ College.py/Exercises/Introdução à Programação de Computadores/Lista de Exercícios/Estrutura de Seleção/bento.py
@@ -21,3 +21,64 @@ to pelo note agora
 tem muito pouca latencia
 é quase instantaneo
 kkkkkkkkkkkkkkkkkkkkkkk
+UDEMY EXERCICES 
+kkkkkkkkkkkkkkkk
+fiz um teste com um arquivo em ruby
+vou 
+pelo pouco que vi parece um pouco com python
+sim simmm kkkkkkkkkk
+queria aprender java 
+mas e dificl pra krl
+eu vi la o print 
+pqp 
+o tamanho da criança 
+KKKKKKKKKKKKKKKKK KKK K novidade 
+cepah q os povo fodao usa essa funçao pra editar codigo e tals 
+em conjunto
+:D
+lembrooo
+apareceu 1 e os karai la 
+parece com python né?
+pelo menos esse pouco
+vai no arquivo de java pra tu ver
+tudo aquilo
+só pra colocar algo na tela 
+kkkkkkkkkkkkkkkkkkkk
+sim simm
+que nem o gustavo mostrou um dia lembra?kkkkkkkkkkkkkk
+o foda q o fernando e o hanai parece q nao curte muito programaçao :C
+:C
+hello darkness my old friend
+fernando é o cara dos números e de arduino
+kkkkkkkkkkkk
+hanai é o cara da rebecca
+KEKW
+kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk novssssssssssssssse
+e eu sou o cara q sofre 
+ASIDAISOD
+mas rlx 
+conheci uma pessoa 
+dale dale 
+ASDJAID
+vamo ver se vai pra frente ne 
+uhuuuu 
+amem irmaos
+kakakakakakkakak
+fala ae
+ kkkkkkkkkkkkkkkkkkkkkkkkkkkkkk aqui e mais dahora 
+ pq estamos programando 
+ print('hugo^')
+print('bento ')
+mee
+meeeeeeeeuuu garoto
+vai que é tua tarafel
+kkkkkkkkkkkkk
+oremos
+mas aí
+deixa eu te perguntar uma coisa
+pq a gente não tá usando o chat?
+OMEGALUL
+"programando"
+vou nem apagar esse arquivo
+vou deixar pra gente ver no final do curso
+as merda que a gente fala
