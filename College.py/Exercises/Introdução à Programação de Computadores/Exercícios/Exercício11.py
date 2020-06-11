91d2b2f325d935ea38251bc397d976d947c75301
Bento Carlos <bcsilva49@gmail.com>
2020-06-10 21:48:45 -0300

    master

--- College.py/Exercises/Introdução à Programação de Computadores/Exercícios/Exercício11.py
+++ College.py/Exercises/Introdução à Programação de Computadores/Exercícios/Exercício11.py
@@ -2,14 +2,12 @@ from random import randint as rd
 
 cartela = [0] * 5
 
-
 for i in range(5):
     cont2 = 0
     cartela[i] = [0] * 5
     for j in range(5):
         cartela[i][j] = 0
           
-
 cont1 = 1
 while cont1 < 6:
     cont2 = 0
