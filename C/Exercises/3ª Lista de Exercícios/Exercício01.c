b899d579216092d00581bdb57cc59cbc283e8d89
Bento Carlos <bcsilva49@gmail.com>
2020-08-06 09:07:58 -0300

    master

--- C/Exercises/3ª Lista de Exercícios/Exercício01.c
+++ C/Exercises/3ª Lista de Exercícios/Exercício01.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    int num, quadrado, raiz;
+    int num, quadrado;
+    double raiz;
     printf("Digite um número: ");
-    scanf("%d", &num);
+    scanf("%lf", &num);
 
     if(num > 0){
         quadrado = num * num;
-        raiz = 
+        raiz = sqrt(num);
+        printf("O quadrado de %d é %d. \nA raíz quadrada de %d é %lf.\n", num, quadrado, num, raiz);
     }
 }
---
+++ College/2ª Semestre/Sistemas Embarcados/Exercícios/proog02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main()  {
+    int cont = 0;
+    float taxa = 1.2, taxa_anual = 1.4;
+    char opcao = 'A';
+    printf("\n Valor de cont= %d", cont);
+    printf("\n Valor de taxa= %f", taxa);
+    printf("\n Valor de taxa_anual= %f", taxa_anual);
+    printf("\n Valor de opcao= %c\n", opcao);
+
+} /*Fim função main*/
