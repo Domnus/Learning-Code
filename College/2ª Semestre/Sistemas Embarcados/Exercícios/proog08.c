ee94334fc0efa6da72b1781e640b97971008d8b9
Bento Carlos <bcsilva49@gmail.com>
2020-08-12 20:34:22 -0300

    master

---
+++ College/2ª Semestre/Sistemas Embarcados/Exercícios/proog08.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main( ) {
+
+   int car = 'A';
+   printf(" O caractere %c \n  ", car);
+   printf(" Eh em decimal %d \n ", car);
+   printf(" e  em hexadecimal %x \n ", car);
+   printf(" Nro. de bytes que ocupa na memoria=%d \n", sizeof(car));
+   
+}/*Fim main*/
