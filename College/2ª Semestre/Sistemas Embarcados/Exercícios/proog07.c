0ef1c567f9b1c6cf1034f308ccf7bd27918b72fb
Bento Carlos <bcsilva49@gmail.com>
2020-08-12 20:32:25 -0300

    master

---
+++ College/2ª Semestre/Sistemas Embarcados/Exercícios/proog07.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main( ) {
+
+	int verdadeiro, falso;
+	verdadeiro = (15 < 20);
+	falso = (15 == 20);
+	printf("\n VERDADEIRO=%d,  FALSO=%d", verdadeiro, falso);
+
+
+}/*Fim main*/
