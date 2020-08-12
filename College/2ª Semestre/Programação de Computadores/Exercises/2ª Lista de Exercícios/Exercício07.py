818601be290ab5c6bfdab4512a8b99a218602a4b
Bento Carlos <bcsilva49@gmail.com>
2020-08-12 14:32:39 -0300

    master

--- College/2ª Semestre/Programação de Computadores/Exercises/2ª Lista de Exercícios/Exercício07.py
+++ College/2ª Semestre/Programação de Computadores/Exercises/2ª Lista de Exercícios/Exercício07.py
@@ -1,11 +1,24 @@
-#include <stdio.h>
+while True:
+    cpf = input("Informe o CPF: ")
+    if not(cpf.isdigit()):
+        print("CPF inválido, digite apenas números")
+    else:
+        break
+d1 = 0
+d2 = 0
+for i in range(9):
+    d1 += (ord(cpf[i])-48)*(i+1)
+    d2 += (ord(cpf[i])-48)*(9-i)
+if d1 % 11 == 10:
+    d1 = 0
+else:
+    d1 %= 11
+if d2 % 11 == 10:
+    d2 = 0
+else:
+    d2 %= 11
 
-int main()
-{
-    char nome[5];
-
-    printf("Seila");
-    scanf("%s", &nome);
-
-    printf("Seu nome é %s\n", nome);
-}
+if d1 == int(cpf[9]) and d2 == int(cpf[10]):
+    print("CPF válido")
+else:
+    print("CPF inválido")
--- College/2ª Semestre/Programação de Computadores/Exercises/Avaliações/Avaliação01.py
+++ College/2ª Semestre/Programação de Computadores/Exercises/Avaliações/Avaliação01.py
@@ -1,5 +1,20 @@
-coca = input("Mensagem codificada: ")
+# Bento Carlos Silva dos Santos RA: 600784
+# Fernando Cremonez Costa RA: 604097
+
+coca = input("Mensagem codificada: ").upper()
 
 coca = list(coca)
 
+for i in range(len(coca)):
+    try:
+        if "P" == coca[i+1]:
+            continue
+    except:
+        Exception
+    else:
+        coca.pop(i)
+        
+coca = "".join(coca)
 print(coca)
+
+ 
