a5afc23f55637d527168b780ea58c7113469f8b9
Bento Carlos <bcsilva49@gmail.com>
2020-08-13 14:33:48 -0300

    master

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,5 +1,7 @@
-public class HelloWorld {
+// Your First Program
+
+class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("Hello, World!");
     }
 }
