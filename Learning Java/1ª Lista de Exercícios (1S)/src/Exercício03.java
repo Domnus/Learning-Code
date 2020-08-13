a4f849f66c8f67d7f067e6d74aea6530b0ed3301
Bento Carlos <bcsilva49@gmail.com>
2020-08-13 17:20:37 -0300

    master

---
+++ Learning Java/1ª Lista de Exercícios (1S)/src/Exercício03.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercício03 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Digite um número em metros:");
+		float metro = scan.nextFloat();
+		
+		float mil = num * 100;
+		
+		System.out.println(metro + " metro(s) em milímetros é igual a " + mil + " milímetro(s).");
+		
+	}
+
+}
