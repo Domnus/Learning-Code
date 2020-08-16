5bf1ef376c55ac5db50bc2b33d5fbc42d8d39491
Bento Carlos <bcsilva49@gmail.com>
2020-08-16 11:10:49 -0300

    master

--- College/C/test.c
+++ College/C/test.c
@@ -1,10 +1,9 @@
-#include <termios.h>
 #include <stdio.h>
 
 int main()
 {
   int x, y, m;
-  int a, b, i, j, k = 0;
+  int a, b = 0, i, j, k = 0;
   
   printf("Insira a range no formato [COMEÇO FIM] -> ");
   scanf("%d %d", &x, &y);
@@ -28,16 +27,36 @@ int main()
 
   for (i = 0; i < a; i++)
   {
+    k = 0;
     for (j = x; j < y; j++)
     {
       if ((j - i) % a == 0)
       {
         list[i][k] = j;
       }
+      else
+      {
+        list[i][k] = 0;
+      }
+      
       k++;
     }
   }
   
-  printf("%d", list);
-
+  for (i = 0; i < a; i++)
+  {
+    printf("[%d]=", i);
+    for (j = 0; j < b; j++)
+    {
+      if (list[i][j] == 0)
+      {
+        continue;
+      }
+      else
+      {
+        printf("[%d]", list[i][j]);
+      }
+    }
+    printf("\n");
+  }
 }
