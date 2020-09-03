b3804f715d7967281fbefefaddee67dd15955093
Bento Carlos <bcsilva49@gmail.com>
2020-09-03 12:13:22 -0300

    master

--- Testes/Testes em C/test02.c
+++ Testes/Testes em C/test02.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    printf("\a\n");
+    int n1, n2;
+    printf("Digite um número: ");
+    scanf("%d", &n1);
+
+    n2 = pow(n1, 2);
+
+    printf("%d ao quadrado é igual a %d.\n", n1, n2);
+    
 }
