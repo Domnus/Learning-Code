86d84e1f37c56687ad3917774eb40a43ea85bcd3
Bento Carlos <bcsilva49@gmail.com>
2020-09-04 20:16:55 -0300

    master

---
+++ College/Machine Learning/webcam2.py
@@ -0,0 +1,44 @@
+import cv2
+
+video = cv2.VideoCapture(0)
+
+background = None
+restart = 0
+
+while True:
+    ret, frame = video.read()
+
+    frame_resize = cv2.resize(frame, (500, 500))
+
+    gray = cv2.cvtColor(frame_resize, cv2.COLOR_BGR2GRAY)
+
+    frame_gaussian = cv2.GaussianBlur(gray, (35, 35), 0)
+
+    if background is None:
+        background = frame_gaussian
+
+    difference = cv2.absdiff(frame_gaussian, background)
+
+    thresh = cv2.threshold(difference, 25, 255, cv2.THRESH_BINARY)[1]
+
+    dilate = cv2.dilate(thresh, None, iterations=2)
+
+    contours = cv2.findContours(
+        
+    dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+    for c in contours:
+        if cv2.contourArea(c) < 1200:
+            continue
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        print("Detectou")
+
+    if restart >= 5:
+        restart = 0
+        background = None
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+    restart += 1
