404b7989f337f8dd82489ebf3ba64fb765bdf868
Domnus <bcsilva49@gmail.com>
2020-09-14 13:06:30 -0300

    master

--- College/Machine Learning/Aula 4/deteccaoFaces.py
+++ College/Machine Learning/Aula 4/deteccaoFaces.py
@@ -1,13 +1,20 @@
 import cv2
 
-imagem = cv2.imread('pessoas.jpg')
+#imagem = cv2.imread('pessoas.jpg')
+imagem = cv2.imread(
+    'D:\\Github\\Learning-Code\\College\\Machine Learning\\Aula 4\\pessoas.jpg')
 
 classificador = cv2.CascadeClassifier(
-    "/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml")
+    "C:\\Users\\Usuario\\AppData\\Local\\Programs\\Python\\Python38-32\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml")    
+#classificador = cv2.CascadeClassifier(
+#    "/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml")
 
 imagemcinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
-deteccoes = classificador.detectMultiScale(imagemcinza)
+deteccoes = classificador.detectMultiScale(imagemcinza, scaleFactor=1.1,
+                                           minNeighbors=5,
+                                           minSize=(30, 30),
+                                           maxSize=(100, 100))
 
 print(deteccoes)
 print(len(deteccoes))
