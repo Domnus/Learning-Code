75542b05573a3e16a110763f05a749a0a1f3b8bf
Bento Carlos <bcsilva49@gmail.com>
2020-09-15 20:24:35 -0300

    master

--- Testes/Testes em Python/teste04.py
+++ Testes/Testes em Python/teste04.py
@@ -1,5 +1,120 @@
-from rich.console import Console
+# Bento Carlos Silva dos Santos RA: 600784
+# Fernando Cremonez Costa RA: 604097
 
-console = Console()
+import numpy as np
+from colorama import Fore
+from colorama import Style
 
-console.print("Where there is a [bold purple]Will[/bold purple] there [u]is[/u] a [i]way[/i].")
+
+def multmatriz(X, Y):
+    c = [0] * len(X)
+    for i in range(len(X)):
+        c[i] = [0] * len(Y[0])
+        for j in range(len(Y[0])):
+            for k in range(len(Y)):
+                c[i][j] += (X[i][k] * Y[k][j])
+    
+    return c
+
+def menu():
+    print(f"{Fore.CYAN}-----Criptografia-----{Style.RESET_ALL}")
+    print(f"{Fore.CYAN}-> 1. Criptografar    {Style.RESET_ALL}")
+    print(f"{Fore.CYAN}-> 2. Descriptografar {Style.RESET_ALL}")
+    print(f"{Fore.CYAN}-> 3. Sair do Programa{Style.RESET_ALL}")
+
+    return int(input("Escolha a opção-> "))
+
+
+def fraseEmNumero(frase):
+    M = []
+    global lista
+    
+    frase = list(frase)
+
+    for i in range(len(frase)):
+        if frase[i] in lista:
+            j = lista.index(frase[i])
+            M.append(j+1)
+
+    if (len(M) / 2) % 2 != 0:
+        M.append(30)
+
+    return M
+
+
+def numeroEmFrase(numeros):
+    global lista
+    bn = descriptografar(numeros)
+    BN = []
+
+    for i in range(len(bn)):
+        for j in range(len(bn[i])):
+            BN.append(bn[i][j])
+
+    for i in range(len(BN)):
+        BN[i] = lista[BN[i]-1]
+
+    BN = ''.join(BN)
+
+    return BN
+
+
+def criptografar(frase):
+    global A
+    m = fraseEmNumero(frase)
+    M = [[], []]
+    N = [[], []]
+    half = len(m) / 2
+    M[0] = m[:int(half)]
+    M[1] = m[int(half):]
+
+    N = multmatriz(A,M)
+
+    return N
+
+
+def descriptografar(n):
+    global B
+    
+    n = n.split(' ')
+    
+    while '' in n:
+        n.remove('')
+
+    for i in range(len(n)):
+        n[i] = int(n[i])
+        
+    N = [[], []]
+    half = len(n) / 2
+    N[0] = n[:int(half)]
+    N[1] = n[int(half):]
+    BN = multmatriz(B, N)
+
+    return BN
+
+
+while True:
+    lista = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+             'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '.', '!', '#', ' ']
+    A = [[3, 1], [2, 1]]
+    B = [[1, -1], [-2, 3]]
+    x = menu()
+    if x == 1:
+        frase = input("Digite a frase-> ").upper().replace(' ', '#')
+        N = criptografar(frase)
+        print(f"{Fore.GREEN}Frase criptografada: {Style.RESET_ALL}")
+        for i in range(len(N)):
+            for j in range(len(N[i])):
+                print(f"{Fore.GREEN} {N[i][j]}{Style.RESET_ALL}",
+                      end=f'{Fore.GREEN} {Style.RESET_ALL}')
+        print()
+    elif x == 2:
+        numeros = input("Digite a frase criptografada no formato [NN, NN, NN]-> ")
+        n = numeroEmFrase(numeros)
+        print(f"{Fore.GREEN}Frase descriptografada: \n{n}{Style.RESET_ALL}")
+    elif x == 3:
+        break
+    else:
+        print(f"{Fore.RED}   _______________________{Style.RESET_ALL}")
+        print(f"{Fore.RED}   |  Entrada Inválida!  |{Style.RESET_ALL}")
+        print(f"{Fore.RED}   -----------------------{Style.RESET_ALL}")
--- Testes/Testes em Python/teste05.py
+++ Testes/Testes em Python/teste05.py
@@ -1,70 +1,7 @@
-import numpy as np
-
-def converte(mensa):
-    novafrase = []
-    for letra in mensa:
-        novafrase.append(lista.index(letra))
-    if len(novafrase) % 2 != 0 : #SE A FRASE É PAR OU IMPAR 
-        novafrase.append(30) # SE FOR IMPAR NAO DA PRA DIVIDIR EM 2 PARTES.
-    return novafrase
-
-def converte2():
-    global lista
-    n2 = Descriptografar()
-    frase = []
-    for i in range(len(n2)):
-        for j in range(len(n2[i])): #VAI PEGAR  A SEGUNDA LISTA
-            frase.append(n2[i][j])
-    
-    for i in range(len(frase)):
-        frase[i] = lista[frase[i]] #COMPARAR E CONVERTER COM A LISTA
-    frase = ''.join(frase) #JUNTAR EM STR     
-    return frase
-
-def criptografar(mensa):
-    global A
-    m = converte(mensa)
-    novafraseM = [[],[]]
-    novafraseN = [[],[]]
-    metade = len(m) // 2
-    novafraseM[0] = m[:int(metade)] #  ':' == COMEÇO ATÉ A METADE
-    novafraseM[1] = m[int(metade):] #  ':' == METADE ATÉ O FINAL
-    novafraseN = np.dot(A,novafraseM)
-
-    return novafraseN
-    
-def Descriptografar(): 
-    global B
-    n = input('Informe os numeros para conversão:')
-    n = n.split(', ')
-    for i in range(len(n)):
-        n[i] = int(n[i])
-    novafraseN = [[],[]]
-    BN = [[],[]]
-    metade = len(n) // 2
-    novafraseN[0] = n[:int(metade)] #  ':' == COMEÇO ATÉ A METADE
-    novafraseN[1] = n[int(metade):] #  ':' == METADE ATÉ O FINAL
-    BN = np.dot(B,novafraseN) #MULTIPLICAR
-
-    return BN
-    
-print('Opção 1 = Criptografar\nOpção 2 = Descriptografar\nOpção 3 = Sair Do programa\n')
-while True:
-    lista = "*ABCDEFGHIJKLMNOPQRSTUVWXYZ.!# "
-    #lista = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '.', '!', '#', ' ']
-    A = [[3,1],[2,1]]
-    B = [[1,-1], [-2,3]]
-    opção = int(input('Informe a opção:'))
-    if opção == 1:
-        mensagem = input("Informe a mensagem:").upper().replace(' ','#')
-        listaNum = criptografar(mensagem)
-        for i in range(len(listaNum)):
-            for j in range(len(listaNum[i])):
-                print(f'{listaNum[i][j]}', end=', ')
-        print()
-    if opção == 2:
-        NumInLetra = converte2()
-        print(NumInLetra)
-    if opção == 3:
-        break
-
+def multmatriz(X, Y):
+    c = [0] * len(X)
+    for i in range(len(X)):
+        c[i] = [0] * len(Y[0])
+        for j in range(len(Y[0])):
+            for k in range(len(Y)):
+                c[i][j] += (X[i][k] * Y[k][j])
