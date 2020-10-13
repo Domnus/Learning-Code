34a3dbde919f650d516ae84967a65637407aed17
Bento Carlos <bcsilva49@gmail.com>
2020-10-13 12:21:28 -0300

    Update Exercício01.py

--- College/2ª Semestre/Programação de Computadores/7ª Lista de Exercícios (2S)/Exercício01.py
+++ College/2ª Semestre/Programação de Computadores/7ª Lista de Exercícios (2S)/Exercício01.py
@@ -1,11 +1,50 @@
-tradutor = {'autismo': 'autism', 'autism': 'autismo'}
+tradutor = {}
 
 
-def buscar(palavra):
+def consulta(palavra):
     if palavra not in tradutor:
-        return "Palavra inválida"
+        return False 
     return tradutor[palavra]
 
+def adicionar(palavra, traducao):
+    if palavra in tradutor or traducao in tradutor:
+        print("Palavra já cadastrada!")
+    else:
+        try:
+            tradutor[palavra] = traducao
+            tradutor[traducao] = palavra
+            return True
+        except: 
+            return False
+
+def listagem():
+    for x, y in tradutor.items():
+        print(f"Palavra: {x} - Tradução: {y}")
+def menu():
+    print(f"-----Dicionário-----\n"
+          f"-> 1. Consultar palavra\n"
+          f"-> 2. Adicionar Palavra\n"
+          f"-> 3. Listagem\n"
+          f"-> 4. Sair do Programa")
+
+    return input("Escolha a opção-> ")
+
+
 while True:
-    x = input("Digite uma palavra para tradução: ")
-    print(buscar(x))
+    op = int(menu())
+    if op == 1:
+        x = input("Digite uma palavra para tradução: ")
+        if not consulta(x):
+            print("Palavra inválida!")
+        else:
+            print(f"Tradução de {x}: {consulta(x)}")
+    elif op == 2:
+        p, t = input("Digita a palavra e a tradução que deseja inserir [PALAVRA, TRADUÇÃO] -> ").split(", ")
+        if adicionar(p,t):
+            print("Tradução adicionada com sucesso!")
+        else:
+            print("Erro na adição da tradução!")
+    elif op == 3:
+        listagem()
+    elif op == 4:
+        break
